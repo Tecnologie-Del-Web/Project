2cb84def799b7fa61d91c9b360de9775bfdc4d8f
HellAmbro <frambrosini1998@gmail.com>
2022-08-05 00:46:50 +0200

    Project skeleton

---
+++ README.md
@@ -0,0 +1,4 @@
+# TechWeb Project official Repository
+## Main working repository for the project
+
+## Documentation
---
+++ include/dbms.inc.php
@@ -0,0 +1,10 @@
+<?php
+
+    $host = "localhost";
+    $user = "root";
+    $pass = "";
+    $name = "tdw_db";
+
+    $mysqli = new mysqli($host, $user, $pass, $name);
+    
+?>
