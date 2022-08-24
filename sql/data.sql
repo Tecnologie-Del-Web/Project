affc6daa4e60220a8abd67fe43d5de549dcc4e35
Luca Di Donato <lucadidonato_2000@libero.it>
2022-08-24 16:50:13 +0200

    Ritoccati relazionale e .sql, ma da controllare

--- sql/data.sql
+++ sql/data.sql
@@ -3,22 +3,22 @@ USE tdw;
 -- Inserisci gli utenti
 INSERT INTO `user` (`user_id`, `name`,`surname`, `phone_number`, `email_address`, `password`) VALUES (1, 'admin', 'admin', '1234567890', 'admin@wolmart.it', '696d29e0940a4957748fe3fc9efd22a3');
 
+-- Inserisco alcune categorie
+INSERT INTO category (category_name, category_description, category_image) VALUES ('Musica', 'CD, DVD, Vinili...', 'category.jpg');
+INSERT INTO category (category_name, category_description, category_image) VALUES ('Sport', 'Palloni, Racchette...', 'category.jpg');
+INSERT INTO category (category_name, category_description, category_image) VALUES ('Libri', 'Romanzi, Fumetti...', 'category.jpg');
+INSERT INTO category (category_name, category_description, category_image) VALUES ('Elettronica', 'Computer, Tablet, Smartphone...', 'category.jpg');
+INSERT INTO category (category_name, category_description, category_image) VALUES ('Abbigliamento', 'Maglieria, Scarpe...', 'category.jpg');
+INSERT INTO category (category_name, category_description, category_image) VALUES ('Cibo e Bevande', 'Cibo e Bevande', 'category.jpg');
+INSERT INTO category (category_name, category_description, category_image) VALUES ('Beauty', 'Shampoo, Saponi...', 'category.jpg');
+INSERT INTO category (category_name, category_description, category_image) VALUES ('Strumenti Musicali', 'Chitarre, Tastiere...', 'category.jpg');
+INSERT INTO category (category_name, category_description, category_image) VALUES ('Cucina', 'Padelle, Pentole, Teglie...', 'category.jpg');
+INSERT INTO category (category_name, category_description, category_image) VALUES ('Mobili', 'Sedie, Tavoli, Letti...', 'category.jpg');
+INSERT INTO category (category_name, category_description, category_image) VALUES ('Neonati', 'Biberon, Ciucci...', 'category.jpg');
+
 -- Inserisco un brand
-INSERT INTO brand (brand_name, website, phone_number, email_address, address) VALUES ('Adelphi', 'adelphi.it', '+39 333', 'adelphi@email.it', 'Via Adelphi, Milano');
-INSERT INTO brand (brand_name, website, phone_number, email_address, address) VALUES ('Einaudi', 'einaudi.it', '+39 3334', 'einaudi@email.it', 'Via Einaudi, Torino');
+INSERT INTO brand (brand_name, website, phone_number, email_address, address, brand_image) VALUES ('Adelphi', 'adelphi.it', '+39 333', 'adelphi@email.it', 'Via Adelphi, Milano', 'brand.jpg');
+INSERT INTO brand (brand_name, website, phone_number, email_address, address, brand_image) VALUES ('Einaudi', 'einaudi.it', '+39 3334', 'einaudi@email.it', 'Via Einaudi, Torino', 'brand.jpg');
 
 -- Inserisco alcuni prodotti
-INSERT INTO product (product_name, price, quantity_available, product_description, brand_id) VALUES ('Il nome della rosa', 15.00, 30, 'Un libro magnifico di Umberto Eco', brand_id);
-
--- Inserisco alcune categorie
-INSERT INTO category (category_name, category_description) VALUES ('Musica', 'CD, DVD, Vinili...');
-INSERT INTO category (category_name, category_description) VALUES ('Sport', 'Palloni, Racchette...');
-INSERT INTO category (category_name, category_description) VALUES ('Libri', 'Romanzi, Fumetti...');
-INSERT INTO category (category_name, category_description) VALUES ('Elettronica', 'Computer, Tablet, Smartphone...');
-INSERT INTO category (category_name, category_description) VALUES ('Abbigliamento', 'Maglieria, Scarpe...');
-INSERT INTO category (category_name, category_description) VALUES ('Cibo e Bevande', 'Cibo e Bevande');
-INSERT INTO category (category_name, category_description) VALUES ('Beauty', 'Shampoo, Saponi...');
-INSERT INTO category (category_name, category_description) VALUES ('Strumenti Musicali', 'Chitarre, Tastiere...');
-INSERT INTO category (category_name, category_description) VALUES ('Cucina', 'Padelle, Pentole, Teglie...');
-INSERT INTO category (category_name, category_description) VALUES ('Mobili', 'Sedie, Tavoli, Letti...');
-INSERT INTO category (category_name, category_description) VALUES ('Neonati', 'Biberon, Ciucci...');
+INSERT INTO product (product_name, price, quantity_available, product_description, brand_id, category_id) VALUES ('Il nome della rosa', 15.00, 30, 'Un libro magnifico di Umberto Eco', 1, 1);
