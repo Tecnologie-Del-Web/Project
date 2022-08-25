60f8aedca470422b3122bbf9fe5f95cdc242c2d9
Luca Di Donato <lucadidonato_2000@libero.it>
2022-08-25 13:30:26 +0200

    Due cosette, poi mi ci rimetto ma serio

--- controllers/product.php
+++ controllers/product.php
@@ -10,8 +10,11 @@ function product()
     $body = new Template($_SERVER['DOCUMENT_ROOT'] . "/skins/frontend/wolmart/product-default.html");
 
     $id = explode('/', $_SERVER['REQUEST_URI'])[2];
+    $brand_id = '';
+    $category_id = '';
 
-    $product = $mysqli->query("SELECT p.product_id, p.product_name, p.price, p.product_description
+
+    $product = $mysqli->query("SELECT p.product_id, p.product_name, p.price, p.product_description, p.brand_id, p.category_id
                                     FROM product p
                                     WHERE p.product_id = $id;");
 
@@ -21,10 +24,47 @@ function product()
         // header("Location: /products");
     } else {
         $product = $product->fetch_assoc();
+        $brand_id = $product['brand_id'];
+        $category_id = $product['category_id'];
         foreach ($product as $key => $value) {
             $body->setContent($key, $value);
         }
     }
+
+
+    $oid = $mysqli->query("SELECT p.product_id, p.product_name, p.price
+                                                FROM product p
+                                                WHERE p.quantity_available > 0 
+                                                  AND p.brand_id = $brand_id
+                                                  AND p.product_id != $id
+                                                ORDER BY p.product_name
+                                                LIMIT 6");
+
+    do {
+        $product = $oid->fetch_assoc();
+        if ($product) {
+            foreach ($product as $key => $value) {
+                $body->setContent("same_brand_" . $key, $value);
+            }
+        }
+    } while ($product);
+
+    $oid = $mysqli->query("SELECT p.product_id, p.product_name, p.price
+                                                FROM product p
+                                                WHERE p.quantity_available > 0 
+                                                  AND p.brand_id != $brand_id
+                                                  AND p.category_id = $category_id
+                                                ORDER BY p.product_name
+                                                LIMIT 3");
+
+    do {
+        $product = $oid->fetch_assoc();
+        if ($product) {
+            foreach ($product as $key => $value) {
+                $body->setContent("other_products_" . $key, $value);
+            }
+        }
+    } while ($product);
 
     $main->setContent("content", $body->get());
     $main->close();
