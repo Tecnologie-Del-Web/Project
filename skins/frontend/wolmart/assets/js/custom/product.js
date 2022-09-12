2026705c2a89af89d62765425ac0042afb3d7d26
Luca Di Donato <lucadidonato_2000@libero.it>
2022-09-12 10:02:18 +0200

    Popup semplice, sporco ma carino

--- skins/frontend/wolmart/assets/js/custom/product.js
+++ skins/frontend/wolmart/assets/js/custom/product.js
@@ -38,7 +38,8 @@ $(document).ready(() => {
         if (rating !== 0) {
             addReview(product_id, rating, review);
         } else {
-            alert("Per favore, inserisci una valutazione!");
+            window.scrollTo(0, 0);
+            addAlert('error', $("#category"), "Per favore, inserisci una valutazione!");
         }
     });
 
@@ -159,4 +160,31 @@ function addReview(product_id, rating, review) {
             }
         }
     });
+}
+
+// Aggiunge un alert con il messaggio passato e lo inserisce dopo il selettore parent passato.
+// L'alert sparisce in automatico dopo X secondi
+function addAlert(type, parent, message) {
+    let color;
+    switch (type) {
+        case 'error':
+            color = '#c02323';
+            break;
+        case 'success':
+            color = '#23c02b';
+            break;
+        default:
+            color = 'black';
+    }
+
+    $("body").after('' +
+        '<div class="alert alert-dismissible fade show m-0 form custom-modal" role="alert" style="color:' + color + ';">'
+        + '<p class="text-primary subtitle">' + message + '</p>' +
+        '<button type="button" class="btn btn-primary w-100 br-sm" data-bs-dismiss="alert" aria-hidden="true" style="display: block; margin: 0 auto;" onclick="$(\'div.alert\').slideUp(200).alert(\'close\');">Ok</button>' +
+        '</div>'
+    );
+
+    setTimeout(function () {
+        $('div.alert').slideUp(200).alert('close');
+    }, 3000);
 }
